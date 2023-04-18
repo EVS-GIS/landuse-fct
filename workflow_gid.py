064781c27ed704365b6f580139aeceee1789ed4f
Louis Manière <louismaniere@orange.fr>
2023-04-18 17:13:15 +0200

    Update workflow_gid.py

--- workflow_gid.py
+++ workflow_gid.py
@@ -20,8 +20,8 @@ processes = params['processes']
 
 # create landuse raster
 multiprocess_landuse_gid(tileset = os.path.join(paths['outputs_dir'], paths['tileset_name']), 
-             gid_start = 4501,
-             gid_end = 5000,
+             gid_start = 5001,
+             gid_end = 5866,
              processes = processes,
              tile_dir = paths['tiles_dir'],
              resolution = params['resolution'],
